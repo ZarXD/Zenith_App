186d081f1d26f7ce7406eef3f4b626361b3e70e0
ZarXD <banghads09@gmail.com>
2025-09-25 20:44:39 +0700

    Quiz Layout

--- lib/tugas/quiz/layout_quiz.dart
+++ lib/tugas/quiz/layout_quiz.dart
@@ -7,47 +7,79 @@ class QuizLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Text("Training")
-            ],
-          ),
-          Row(
-            children: [
-              Text("Your Program"),
-              Text("Details")
-            ],
-          ),
-          Container(
-            width: double.infinity,
-            height: 200,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadiusDirectional.circular(30),
-              gradient: LinearGradient(
-                colors: [
-                  Colors.purpleAccent,
-                  Colors.purpleAccent.withOpacity(0.7)
-                ],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight
-              )
+      backgroundColor: Colors.grey.withOpacity(0.1),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(children: [Text("Training")]),
+            SizedBox(height: 20),
+            Row(children: [Text("Your Program"), Text("Details")]),
+            SizedBox(height: 20),
+            Container(
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadiusDirectional.circular(30),
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.purpleAccent,
+                    Colors.purpleAccent.withOpacity(0.7),
+                  ],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+              ),
             ),
-          ),
-          Container(
-            width: double.infinity,
-            height: 100,
-            decoration: BoxDecoration(
-              color: Colors.amber,
-              image: DecorationImage(
-                image: AssetImage("images/card.jpg"),
-                fit: BoxFit.contain
-                )
+            SizedBox(height: 20),
+            Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                      image: AssetImage("images/card.jpg"),
+                      fit: BoxFit.cover                    
+                    ),
+                  ),
+                ),
+              ],
             ),
-          )
-        ],
-      )
+            SizedBox(height: 20),
+            Text("Area of Focus"),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      image: DecorationImage(image: AssetImage("images/ex3.png")),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      image: DecorationImage(image: AssetImage("images/ex4.png")),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
